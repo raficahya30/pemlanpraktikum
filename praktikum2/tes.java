b58146ed5095a3a2a7386ff20502e578e5a9621d
raficahya30 <rafiincome13@gmail.com>
2024-02-23 08:38:21 +0700

    berubah

--- praktikum2/tes.java
+++ praktikum2/tes.java
@@ -2,6 +2,6 @@ package praktikum2;
 
 public class tes {
     public static void main(String[] args) {
-        System.out.println("halo semua!");
+        System.out.println("halo beb love u");
     }
 }
